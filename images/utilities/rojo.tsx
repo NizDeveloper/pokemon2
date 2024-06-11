a7992f77e49c73cbd264703e83d4bc168e8d4e20
Níz Peña <nizadiel5@gmail.com>
2024-06-11 10:18:17 -0700

    Add p1 files

--- images/utilities/rojo.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Cuadro-Rojo" tilewidth="32" tileheight="32" tilecount="121" columns="11">
- <image source="../../../../../Desktop/Cuadro-Rojo.png" width="376" height="376"/>
-</tileset>
